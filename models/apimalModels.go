e8dd880446e21b55bc8cb10ed371fdeb128a519a
hisyntax <hisyntax0@gmail.com>
2021-12-14 12:14:23 +0100

    models all set

---
+++ models/apimalModels.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Animal struct {
+	ID                  primitive.ObjectID `bson:"_id"`
+	Name                string             `json:"name"`
+	Description         string             `json:"description"`
+	Image               string             `json:"image"`
+	Habitat             []string           `json:"habitat"`
+	Domain              string             `json:"domain"`
+	Kingdom             string             `json:"kingdom"`
+	Phylum              string             `json:"phylum"`
+	Class               string             `json:"class"`
+	Order               string             `json:"order"`
+	Family              string             `json:"family"`
+	Genus               string             `json:"genus"`
+	Specie              string             `json:"specie"`
+	Color               string             `json:"color"`
+	FoodType            []string           `json:"food_type"`
+	OvuationPeriod      string             `json:"ovulation_period"`
+	GestationPeriod     string             `json:"gestation_period"`
+	EstimatedPopulation int                `json:"extimated_population"`
+	ExtinctionStatus    string             `json:"extinction_status"`
+	Reproduction        string             `json:"reproduction"`
+	Motility            string             `json:"motility"`
+	MatingSeason        string             `json:"mating_season"`
+	ModeOfBirth         string             `json:"mode_of_birth"`
+}
